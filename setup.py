859f4fdd8ff2fe99c3b691da80699c7ed7094337
Joel Klinger <joel.alexander.klinger@gmail.com>
2021-05-05 14:08:50 +0100

    added autopypi deploy and license

--- setup.py
+++ setup.py
@@ -1,35 +1,31 @@
 from setuptools import setup
 from nuts_finder import __version__
 
-version = ''.join(v for v in __version__ if (v.isnumeric() or v == '.'))
+version = "".join(v for v in __version__ if (v.isnumeric() or v == "."))
 
 
 common_kwargs = dict(
     version=version,
-    license='MIT',
-    install_requires=["geojson==2.5.0",
-                      "requests==2.22.0",
-                      "Shapely==1.6.2.post1"],
-    long_description=open('README.md').read(),
+    license="MIT",
+    install_requires=["geojson==2.5.0", "requests==2.22.0", "Shapely==1.6.2.post1"],
+    long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url='https://github.com/nestauk/nuts_finder',
-    author='Joel Klinger',
-    author_email='joel.klinger@nesta.org.uk',
-    maintainer='Joel Klinger',
-    maintainer_email='joel.klinger@nesta.org.uk',
+    url="https://github.com/nestauk/nuts_finder",
+    author="Nesta",
+    author_email="software_development@nesta.org.uk",
+    maintainer="Nesta",
+    maintainer_email="software_development@nesta.org.uk",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7'
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
     ],
-    python_requires='>3.6',
+    python_requires=">3.6",
     include_package_data=True,
 )
 
-setup(name='nuts_finder',
-      packages=['nuts_finder'],
-      **common_kwargs)
+setup(name="nuts_finder", packages=["nuts_finder"], **common_kwargs)
